e92bfe1c022b36122e47b019ad4019f9fae4d382
Jason Hurt <jleehurt@gmail.com>
2009-07-25 17:10:41 -0700

    first commit

---
+++ src/PerceptronHaykin.clj
@@ -0,0 +1,155 @@
+;;Copyright (c) 2009, University of Nevada, Las Vegas
+;;All rights reserved.
+;;
+;;Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+;;
+;;    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+;;    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+;;    * Neither the name of the University of Nevada, Las Vegas, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+;;
+;;THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;; an implementation of the perceptron convergence algorithm as described in:
+;; S. Haykin: Neural Networks: A Comprehensive Foundation, Prentice Hall, 1999, pp 142-143
+
+;; Neuron Activation Functions
+
+;threshold
+(defn threshold [x] (if (>= x 0.0) 1.0 0.0))
+
+;signum (threshold)
+;(defn signum [x] (cond (> x 0.0) 1.0 (= x 0.0) 0.0 (< x 0.0) -1.0))
+(defn signum [x] (cond (> x 0.0) 1.0 (<= x 0.0) -1.0))
+
+;piecewise linear
+(defn piecewise [x] (cond (>= x 0.5) 1.0 (and (>  x -0.5) (< x 0.5)) x (<= x -0.5) 0.0))
+
+;logistic (sigmoidal)
+(defn sigmoid [x slopeParameter] (/ 1.0 (+ 1.0 (Math/exp (* -1.0 (* x slopeParameter))))))
+
+;hyberbolic tangent (sigmoidal)
+(defn hyperbolicTangent [x] (Math/tanh x))
+
+;arctangent (sigmoidal)
+(defn arcTangent [x] (Math/atan x))
+
+;gompertz curve (sigmoidal)
+; a is the upper asymptote
+; c is the growth rate
+; b, c are negative numbers
+(defn gompertzCurve [x a b c] (* a (Math/pow Math/E (* b (Math/pow Math/E (* c x))))))
+
+;algebraic sigmoid
+(defn algebraicSigmoid [x] (/ x (Math/sqrt (+ 1.0 (Math/pow x 2.0)))))
+
+;; Matrix Functions
+
+(defn transpose [matrix]
+  (if (not (nil? matrix))
+      (apply map list matrix)))
+
+(defn transpose2 [matrix]
+  (apply map (fn [& column] column) matrix))
+
+(defn matrixMultiply [matrixA matrixB]
+  (map
+    (fn [row] (apply map (fn [& column] (apply + (map * row column))) matrixB))
+    matrixA))
+
+(defn matrixAdd [matrixA matrixB]
+  (if (and (not (empty? matrixA)) (not (empty? matrixB)))
+      (conj
+        (matrixAdd (rest matrixA) (rest matrixB))
+        (map + (first matrixA) (first matrixB)))))
+
+(defn matrixMultiplyScalar [matrixA scalar]
+  (if (not (empty? matrixA))
+      (conj
+        (matrixMultiplyScalar (rest matrixA) scalar)
+        (map (fn [arg] (* arg scalar)) (first matrixA)))))
+
+;; Vector Functions
+
+(defn transposeVector [v]
+  (if (not (nil? v))
+      (transpose (vector v))))
+
+(defn vectorMultiplyScalar [v scalar]
+  (map * v (cycle [ scalar ])))
+
+;; Binary Logic Input/Output
+
+(def infiniteInputCollection (cycle [[[1.0 1.0 1.0]] [[1.0 -1.0 1.0]] [[1.0 1.0 -1.0]] [[1.0 -1.0 -1.0]]]))
+(def infiniteAndOutputCollection (cycle [1.0 -1.0 -1.0 -1.0]))
+
+(defn buildInputs [numberOfInputs]
+  (loop [inputVector []
+         binaryInputCollection infiniteInputCollection
+         remainingCount numberOfInputs]
+        (if (> 0 remainingCount)
+            inputVector
+            (recur
+              (conj inputVector (first binaryInputCollection)) (rest binaryInputCollection) (dec remainingCount)))))
+
+(defn buildOutputs [numberOfOutputs outputCollection]
+  (loop [outputVector []
+         andOutputCollection outputCollection
+         remainingCount numberOfOutputs]
+        (if (> 0 remainingCount)
+            outputVector
+            (recur (conj outputVector (first andOutputCollection)) (rest andOutputCollection) (dec remainingCount)))))
+
+;; Main
+
+;learning rate parameter, eta 0 < eta <= 1
+(def learningRateParameter 1.0)
+
+;the weight vector of the perceptron
+;(def weightVector (ref nil))
+
+;multiply the transpose of the weight vector with the input vector
+;apply the signum function to the scalar result
+(defn computeActualResponse [signumFunction weights inputs]
+  (if (and (not (nil? weights)) (not (nil? inputs)))
+      ;;TODO create a function that will apply first to a collection until the inner item is obtained
+      ;use a doall to force evaluation to avoid overflowing the stack
+      (signumFunction (first (first (doall (matrixMultiply (transpose weights) inputs)))))))
+
+;return an updated weight vector of the perceptron
+(defn getAdaptedWeightVector [weights inputs desiredResponse actualResponse]
+  (let [etaDeltaDesiredActual (* learningRateParameter (- desiredResponse actualResponse))]
+    (println "adapted weight vector" (matrixAdd weights (matrixMultiplyScalar inputs etaDeltaDesiredActual)))
+    (println "desiredResponse" desiredResponse)
+    (println "actualResponse" actualResponse)
+    (println "learningRateParameter" learningRateParameter)
+    (println "etaDeltaDesiredActual" etaDeltaDesiredActual)
+    (println "weights" weights)
+    (println "inputs" inputs "\n")
+    (if (not (= 0.0 etaDeltaDesiredActual))
+      (cons 1 (rest (matrixAdd weights (matrixMultiplyScalar inputs etaDeltaDesiredActual))))
+      weights)))
+
+;train the perceptron with the inputs and corresponding known outputs
+(defn trainPerceptron [beginningWeightVector allInputs allOutputs]
+  (loop [weightVector beginningWeightVector
+         inputs allInputs
+         responses allOutputs]
+        (if (and (not (empty? inputs)) (not (empty? responses)))
+            (let [adaptedWeightVector
+                  (getAdaptedWeightVector
+                    weightVector
+                    (first inputs)
+                    (first responses)
+                    (computeActualResponse signum weightVector (first inputs)))]
+                 (recur adaptedWeightVector (rest inputs) (rest responses)))
+            weightVector)))
+
+(defn main [sizeOfDataSet]
+  (let [weights [[1.0 0.5 0.5]]
+        inputs (buildInputs sizeOfDataSet)
+        outputs (buildOutputs sizeOfDataSet infiniteAndOutputCollection)]
+       (trainPerceptron weights inputs outputs)))
+
+(defn classifyInput [input weights]
+  (computeActualResponse signum weights input))
+  
---
+++ src/PerceptronRojas.clj
@@ -0,0 +1,60 @@
+;;Copyright (c) 2009, University of Nevada, Las Vegas
+;;All rights reserved.
+;;
+;;Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+;;
+;;    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+;;    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+;;    * Neither the name of the University of Nevada, Las Vegas, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+;;
+;;THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;; an implementation of the perceptron convergence algorithm as
+;; described in R. Rojas: Neural Networks, Springer-Verlag, Berlin, 1996, pp 85
+
+;the weights of the vector, this will be set during upon completion of the training of the perceptron
+(def theWeights (ref []))
+
+(def AND-table {[0 0 1] 0
+                [0 1 1] 0
+                [1 0 1] 0
+                [1 1 1] 1})
+
+(defn getWeights [numberOfWeights]
+  (repeat numberOfWeights 0.5))
+
+(defn transposeArray [array]
+  (map (fn [& column] column) array))
+
+(defn multiplyScalar [array scalar]
+  (map * (repeat (count array) scalar) array))
+
+(defn weightsTransposeByInputs [weights inputs]
+  (reduce + (map * (map first (transposeArray weights)) inputs)))
+
+(defn weightsLessInputs [weights inputs]
+  (map - weights inputs))
+
+(defn weightsPlusInputs [weights inputs]
+  (map + weights inputs))
+
+(defn correctAndWeights [initialWeights initialInputs initialNumberCorrect]
+  (loop [weights initialWeights
+         inputs initialInputs
+         numberCorrect initialNumberCorrect]
+    (println "current weights: " weights " current input: " (first inputs))
+    ;terminating condition: all of the inputs have been correctly classified
+    (if (= (count AND-table) numberCorrect)
+      weights
+      (if (and (<= 0 (weightsTransposeByInputs weights (first inputs))) (= 1 (AND-table (first inputs))))
+          (recur (weightsLessInputs weights (first inputs)) (rest inputs) 0)
+          (if (and (>= 0 (weightsTransposeByInputs weights (first inputs))) (= 0 (AND-table (first inputs))))
+              (recur (weightsPlusInputs weights (first inputs)) (rest inputs) 0)
+              (recur weights (rest inputs) (inc numberCorrect)))))))
+
+(defn trainAndWeights []
+  (dosync
+    (ref-set theWeights (correctAndWeights (getWeights (count (first (keys AND-table)))) (cycle (keys AND-table)) 0))))
+
+(defn classifyInput [input]
+  (if (> 0 (weightsTransposeByInputs (deref theWeights) input)) 1 0))
