663abdce770a6f56fb10c0ee7126ecab84cb8601
Jason Hurt <jleehurt@Jason-Hurts-iMac.local>
2010-01-24 11:59:30 -0800

    Move example jxta code to a separate package.

--- run-pipe-client.sh
+++ run-pipe-client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CLOJURE_DIR=/Users/jleehurt/Dev/clojure-read-only
-java -cp nn.jar:lib/jxta/bcprov-jdk14.jar:lib/jxta/javax.servlet.jar:lib/jxta/jxta.jar:lib/jxta/org.mortbay.jetty.jar:$CLOJURE_DIR/clojure.jar com.jhurt.p2p.PipeClient
+java -cp nn.jar:lib/jxta/bcprov-jdk14.jar:lib/jxta/javax.servlet.jar:lib/jxta/jxta.jar:lib/jxta/org.mortbay.jetty.jar:$CLOJURE_DIR/clojure.jar com.jhurt.example.PipeClient
