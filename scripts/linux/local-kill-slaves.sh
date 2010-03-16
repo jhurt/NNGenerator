6cefcda1369a8f1e4b1d3c785f1bfdf491c6880a
Jason Hurt <jleehurt@gmail.com>
2010-03-15 19:29:15 -0700

    begin making scripts run on OSX and Linux

--- scripts/linux/local-kill-slaves.sh
+++ scripts/linux/local-kill-slaves.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-ps | grep 'com.jhurt.p2p.SlaveR' | awk '{print $1}' | xargs kill -9
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   platform='freebsd'
+fi
+
+echo $unamestr
+
+
+#ps | grep 'com.jhurt.p2p.SlaveR' | awk '{print $1}' | xargs kill -9
+
+#ps -aef | grep com.jhurt.p2p.SlaveR | awk '{print $2}' | xargs kill -9
