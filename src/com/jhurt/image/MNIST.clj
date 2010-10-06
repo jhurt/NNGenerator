1fda4e572ca441e09d7a7964227b0a5ef776c6ca
Guest <guest@jhurt-mbp.local>
2010-10-05 19:38:49 -0700

    add functions for working with OCR data from the MNIST database

---
+++ src/com/jhurt/image/MNIST.clj
@@ -0,0 +1,57 @@
+;;Copyright (c) 2010, University of Nevada, Las Vegas
+;;All rights reserved.
+;;
+;;Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+;;
+;;    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+;;    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+;;    * Neither the name of the University of Nevada, Las Vegas, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+;;
+;;THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;;Functions for extracting data from label and image files from the MNIST database
+;;The file formats are described here: http://yann.lecun.com/exdb/mnist/
+
+(ns
+  #^{:author "Jason Lee Hurt"}
+  com.jhurt.image.MNIST
+  (:require clojure.java.io))
+
+(import
+  '(java.awt.image BufferedImage ImageObserver FilteredImageSource)
+  '(java.io DataInputStream File)
+  '(javax.imageio ImageIO)
+  '(java.awt Graphics))
+
+(defn- extractLabels [stream count]
+  (loop [labels [] c count]
+    (if (= 0 c)
+      (do (.close stream) labels)
+      (recur (conj labels (.readUnsignedByte stream)) (dec c)))))
+
+(defn readLabels
+  "read the labels from an idx file from the MNIST database"
+  [labelFile]
+  (let [stream (new DataInputStream (input-stream labelFile))
+        count (.readInt stream)]
+    ;pop the magic number
+    (.readInt stream)
+    (extractLabels stream count)))
+
+(defn- extractImages [stream count width height]
+  (loop [images [] rows [] row [] c count w 0 h 0]
+    (cond
+      (= 0 c) (do (.close stream) images)
+      (= w width) (recur images (conj rows row) [] c 0 (inc h))
+      (= h height) (recur (conj images rows) [] [] (dec c) 0 0)
+      :else (recur images rows (conj row (.readUnsignedByte stream)) c (inc w) h))))
+
+(defn readImages
+  "read the digit images from an idx file from the MNIST database"
+  [imageFile]
+  (let [stream (new DataInputStream (input-stream imageFile))
+        magic (do (.readInt stream))
+        total (do (.readInt stream))
+        width (do (.readInt stream))
+        height (do (.readInt stream))]
+    (extractImages stream total width height)))
